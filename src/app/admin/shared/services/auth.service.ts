d3f2d1482a514253a0c14724f012cc6bf1a21027
OnoprienkoArtem <o.artem.e@gmail.com>
2020-02-13 22:46:17 +0200

    store token and expires

--- src/app/admin/shared/services/auth.service.ts
+++ src/app/admin/shared/services/auth.service.ts
@@ -11,7 +11,12 @@ export class AuthService {
   constructor(private http: HttpClient) { }
 
   get token(): string {
-    return ''
+    const expDate = new Date(localStorage.getItem('fb-token-exp'));
+    if (new Date() > expDate) {
+      this.logout();
+      return null;
+    }
+    return localStorage.getItem('fb-token');
   }
 
   login(user: User): Observable<any> {
@@ -23,18 +28,23 @@ export class AuthService {
   }
 
   logout() {
-
+    this.setToken(null);
   }
 
   isAuthenticated(): boolean {
     return !!this.token;
   }
 
-  private setToken(response: FbAuthResponse) {
-    console.log(response);
-    const expDate = new Date(new Date().getTime() + +response.expiresId * 1000);
-    localStorage.setItem('fb-token', response.idToken);
-    localStorage.setItem('fb-token-exp', expDate.toString());
+  private setToken(response: FbAuthResponse | null) {
+    if (response) {
+      console.log(response);
+      const expDate = new Date(new Date().getTime() + +response.expiresId * 1000);
+      localStorage.setItem('fb-token', response.idToken);
+      localStorage.setItem('fb-token-exp', expDate.toString());
+    } else {
+      localStorage.clear();
+    }
+
   }
 
 }
