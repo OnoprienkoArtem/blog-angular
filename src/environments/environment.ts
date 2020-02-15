5328ec371374df8e1d15837a01934fd1661ebcdc
OnoprienkoArtem <o.artem.e@gmail.com>
2020-02-15 19:59:38 +0200

    add posts service

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -6,7 +6,8 @@ import { Environment } from './interface';
 
 export const environment: Environment = {
   production: false,
-  apiKey: ''
+  apiKey: '',
+  fbDbUrl: 'https://angular-blog-6d9fb.firebaseio.com/'
 };
 
 /*
